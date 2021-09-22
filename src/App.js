39970bc05ec7032de1c2c4cd1a44b7f04f181ec3
Ido Agassi <ido.agassi@gmail.com>
2021-09-23 02:43:33 +0300

    added backgrounds

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Chalender from './components/calendar/calendar'
+import Navbar from './components/calendar/navbar/navbar'
+import waves from './blob-scene-haikei.svg';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${waves})` }}>
       <div className="main-section">
+        <Navbar />
         <Chalender/>
       </div>
     </div>
