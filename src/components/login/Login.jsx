b4a852f86546fc25b1762ab435c0197e9c21af91
Jonatan Martens <jonatanmartenstav@gmail.com>
2021-09-23 03:50:07 +0300

    Store course information in local storage

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,27 +1,58 @@
+import axios from "axios";
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import "./Login.css";
 const { Button, TextField } = require("@mui/material");
 
 export default function Login() {
+  const [courseName, setCourseName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    try {
+      const headers = {
+        headers: { "Content-Type": "application/json" },
+      };
+
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_ADDRESS}/courses/login`,
+        {
+          name: courseName,
+          password: password,
+        },
+        headers
+      );
+      localStorage.setItem("course", JSON.stringify(response.data));
+
+      history.push({ pathname: "/Base" });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const history = useHistory();
 
   return (
     <div className="login">
       <h1>Shotgun</h1>
 
-      <TextField className="textbox" label="Course" variant="outlined" />
+      <TextField
+        className="textbox"
+        label="Course"
+        variant="outlined"
+        value={courseName}
+        onChange={(event) => setCourseName(event.target.value)}
+      />
       <TextField
         className="textbox"
         type="password"
         label="Password"
         variant="outlined"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
       />
 
-      <Button
-        className="loginButton"
-        variant="contained"
-        onClick={() => history.push({ pathname: "/Base" })}
-      >
+      <Button className="loginButton" variant="contained" onClick={login}>
         Login
       </Button>
 
